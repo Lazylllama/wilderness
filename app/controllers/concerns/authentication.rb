518bdb50a611948582c6fda58ace447ed2114523
Carlson <dinosaur890123@gmail.com>
2026-08-04 18:21:57 +1000

    feat(auth): added user data and fixed up a previous issue

---
+++ app/controllers/concerns/authentication.rb
@@ -0,0 +1,28 @@
+module Authentication
+  extend ActiveSupport::Concern
+  include do
+    helper_method :current_user,:signed_in?
+  end
+
+  private
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = session[:user_id] && User.find_by(id:session[:user_id])
+  end 
+
+    def signed_in?
+    current_user.present?
+  end
+
+  def sign_in(user)
+    reset_session
+    session[:user_id] = user.id
+    @current_user = user
+  end
+
+  def sign_out
+    reset_session
+    @current_user = nil
+  end
+
+end
--- app/models/users.rb
+++ app/models/users.rb
@@ -7,7 +7,12 @@ class User < ApplicationRecord
     user.first_name = auth.info.first_name
     user.last_name = auth.info.last_name
     user.slack_id = auth.info.slack_id
-    user.verification_status = auth.info.verification_status
+    user.verification_status = auth.info.verification_status.to_s
     user.save!
     user
     end
+
+    def display_name
+    name.presence || first_name.presence|| email.split("@").first
+  end
+end
