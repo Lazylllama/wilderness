1c669c7342d66b11d7de91f67de4a1f9a64143db
Carlson <dinosaur890123@gmail.com>
2026-08-04 16:19:20 +1000

    Update users.rb

--- app/models/users.rb
+++ app/models/users.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
-    def self
+    def self.from_omniauth(auth)
+    user = find_or_initialize_by(hc_uid: auth.uid)
+
+    user.email = auth.info.email
+    user.name = auth.info.name
+    user.first_name = auth.info.first_name
+    user.last_name = auth.info.last_name
+    user.slack_id = auth.info.slack_id
+    user.verification_status = auth.info.verification_status
+    user.save!
+    user
+    end
