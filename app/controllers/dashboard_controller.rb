af01deb0f9395f22e35648952d7c5231701766ee
dinosaur890123 <dinosaur890123@gmail.com>
2026-08-12 22:20:08 +1000

    feat: hackatime integration for dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,12 +2,30 @@ class DashboardController < InertiaController
   before_action :require_authentication
 
   def index
-    render inertia: {
+    render inertia: "camp/index", props: {
         camp: {
-        logs: 0,
-        streak: 0,
-        tents: []
-      }
+          total_hours: tents.sum(&:hours).round(1),
+          fire_state: urrent_user.fire_state,
+          streak: current_user.streak,
+          logs_balance: current_user.logs_balance,
+          plot_count: 8
+        }
+        tents: tents.map {|tent| tent_props(tent)},
+        hackatime_projects: hackatime_projects(tents)
     }
   end
+
+  private 
+  def tent_props(tent)
+    tent.as_json(only: %i[id name description repo_url demo_url hackatime_projects status plot_index last_heartbeat_at hackatime_synced_at shipped_at])
+    .merge("hours"=> tent.hours.round(1), "logs"=> tent.logs, "heat_tier" => tent.heat_tier)
+  end
+
+  def hackatime_projects(tents)
+    owner = tents.flat_map{|tent| tent.hackatime_projects.map {|name|}}.to_h
+
+    (current_user.hackatime_snapshot || []).map do |project|project.slice("name", "total_seconds").merge("claimed_by"=> owner[project["name"]])
 end
+end
+end
+
