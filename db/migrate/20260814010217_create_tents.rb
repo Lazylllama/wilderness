f0235042b30fb69b3c726a01be2a3e5b0d1718eb
Carlson <dinosaur890123@gmail.com>
2026-08-14 04:43:18 +0000

    fix: null constraints

--- db/migrate/20260814010217_create_tents.rb
+++ db/migrate/20260814010217_create_tents.rb
@@ -6,11 +6,11 @@ class CreateTents < ActiveRecord::Migration[8.1]
       t.text :description
       t.string :repo_url
       t.string :demo_url
-      t.json :hackatime_projects, null:false, default: []
-      t.integer :hackatime_seconds, default: 0
+      t.json :hackatime_projects, null: false, default: []
+      t.integer :hackatime_seconds, null: false, default: 0
       t.datetime :last_heartbeat_at
       t.datetime :hackatime_synced_at
-      t.string :status, default: "pitched"
+      t.string :status, null: false, default: "pitched"
       t.integer :plot_index
       t.datetime :shipped_at
       t.timestamps
