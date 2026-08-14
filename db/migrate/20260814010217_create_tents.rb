62635978cf95a0b4b684ced5dff50c84b574e61a
Carlson <dinosaur890123@gmail.com>
2026-08-14 03:49:18 +0000

    feat: add tents table with necessary fields and unique index

--- db/migrate/20260814010217_create_tents.rb
+++ db/migrate/20260814010217_create_tents.rb
@@ -1,7 +1,20 @@
 class CreateTents < ActiveRecord::Migration[8.1]
   def change
     create_table :tents do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name, null: false
+      t.text :description
+      t.string :repo_url
+      t.string :demo_url
+      t.json :hackatime_projects, null:false, default: []
+      t.integer :hackatime_seconds, default: 0
+      t.datetime :last_heartbeat_at
+      t.datetime :hackatime_synced_at
+      t.string :status, default: "pitched"
+      t.integer :plot_index
+      t.datetime :shipped_at
       t.timestamps
     end
+    add_index :tents, [:user_id, :plot_index], unique: true
   end
 end
